6424e820299b70cd1dedec7cbf131b3412d4c9a7
Christopher Wetherill <git@tbmh.org>
2023-07-29 13:52:55 -0400

    Initial commit of minio k8s manifest
    This is a very rough initial commit that sets up a local k8s
    cluster with minikube and deploys a minimal Minio installation
    onto it with kubectl/skaffold.
    
    There's a couple things I need to circle back to and fix:
      - the minio deployment's  PVC is commented out and needs validated;
      - forwarding the service port is wonky and I need to dust off
        some k8s cobwebs and figure out why;
      - I need to play around a bit with profile-specific namespaces in
        Skaffold. I'm doing something not quite right and kubectl is
        barking at me because of it.

---
+++ dstk-infra/bootstrap.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Convenience functions
+info () {
+  printf "\r[ \033[00;34m..\033[0m ] $1\n"
+}
+
+success () {
+  printf "\r\033[2K[ \033[00;32mOK\033[0m ] $1\n"
+}
+
+fail () {
+  printf "\r\033[2K[\033[0;31mFAIL\033[0m] $1\n"
+}
+
+check_binary () {
+  which $1 > /dev/null 2>&1
+  status=$?
+  if [ $status -eq 0 ]; then
+    success "Found binary ${1} in PATH"
+  else
+    fail "No valid binary for ${1} found in PATH. Please correct your installation"
+    exit 1
+  fi
+}
+
+
+# Setup checks
+set +e
+
+info "Checking for Docker installation..."
+check_binary "docker"
+
+info "Checking for minikube installation..."
+check_binary "minikube"
+
+info "Checking for kubectl installation"
+check_binary "kubectl"
+
+info "Checking for skaffold installation"
+check_binary "skaffold"
+
+set -e
+
+# Minikube startup
+minikube dev --profile dstk-dev
