407be4905e46f9409e407c0eddb7b56a8512b148
Christopher Wetherill <1910269+wetherc@users.noreply.github.com>
2023-08-03 19:07:19 +0000

    Add Apollo Server, flatten infra folder structure (#11)
    Subfolders were getting annoying so I just flattened things
    a little bit.
    
    More substantively, this commit adds a skeleton Apollo Server
    service. Note that it currently will error out, but that's known
    and only because I'm not defining and exporting any types in
    `apollo/graphql`. That's an easy fix. As we get the DB tables
    stood up, I'll start adding the GQL queries and mutations.

---
+++ apollo/src/index.ts
@@ -0,0 +1,51 @@
+import { makeSchema } from 'nexus'
+import { Request } from 'express'
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
+import { Model } from 'objection'
+import Knex from 'knex'
+import { knexConfig } from './knexfile.js'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import * as types from './graphql/index.js'
+
+
+const knex = Knex(knexConfig.development)
+Model.knex(knex)
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const schema = makeSchema({
+  types,
+  outputs: {
+    schema: path.join(__dirname, '../schema.graphql'),
+    typegen: path.join(__dirname, 'typegen.ts'),
+  },
+  // sourceTypes: {
+  //   modules: [
+  //     {
+  //       module: path.join(__dirname, 'typeDefs.js'),
+  //       alias: 't',
+  //     },
+  //   ],
+  // },
+  contextType: {
+    module: path.join(__dirname, 'context.js'),
+    export: 'Context',
+  },
+})
+
+const context = async ({ req }: { req: Request }) => {
+  // simple auth check on every request
+  const auth = (req.headers && req.headers.authorization) || ''
+  return null
+}
+
+const server = new ApolloServer({
+  schema,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
