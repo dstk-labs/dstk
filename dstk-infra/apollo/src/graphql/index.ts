71a271ca794bbe271a100737740b83aff0abbfec
StephenODea54 <odeastephen1@gmail.com>
2023-08-07 17:14:57 -0400

    Nexus & Objection for `dstk_registry.registry_storage_providers`

--- dstk-infra/apollo/src/graphql/index.ts
+++ dstk-infra/apollo/src/graphql/index.ts
@@ -1,1 +1,2 @@
 export * from './model/model.js';
+export * from './storage-provider/storageProvider.js';
---
+++ dstk-infra/apollo/src/graphql/storage-provider/storageProvider.ts
@@ -0,0 +1,38 @@
+import { objectType } from 'nexus';
+import { Model } from 'objection';
+
+export const StorageProvider = objectType({
+    name: 'StorageProvider',
+    definition(t) {
+        t.id('providerId');
+        t.string('endpointUrl');
+        t.string('region');
+        t.string('bucket');
+        t.string('accessKeyId');
+        t.string('secretAccessKey');
+        t.string('createdBy'); // TODO: resolve actual user object
+        t.string('modifiedBy');
+        t.string('owner');
+        t.string('dateCreated');
+        t.string('dateModified');
+    },
+});
+
+export class ObjectionStorageProvider extends Model {
+    id!: string;
+    endpointUrl!: string;
+    region!: string;
+    bucket!: string;
+    accessKeyId!: string;
+    secretAccessKey!: string;
+    createdBy!: string;
+    modifiedBy!: string;
+    owner!: string;
+    dateCreated!: string;
+    dateModified!: string;
+
+    static TableName = 'registryStorageProviders';
+    static get idColumn() {
+        return 'providerId';
+    }
+}
