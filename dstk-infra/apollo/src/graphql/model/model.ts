055cae16b182e63299f9c01eddf982432e2adffd
Stephen O'Dea <odeastephen1@gmail.com>
2023-08-10 14:44:22 -0400

    Add model and storage provider relationship (#17)
    * Add model and storage provider relationship
    
    * change registry_model foreign key reference in storage_provider table
    
    * Remove storage provider object from model relation

--- dstk-infra/apollo/src/graphql/model/model.ts
+++ dstk-infra/apollo/src/graphql/model/model.ts
@@ -1,36 +1,42 @@
-import { objectType } from 'nexus'
-import { Model } from 'objection'
+import { objectType } from 'nexus';
+import { Model } from 'objection';
+import { ObjectionStorageProvider } from '../storage-provider/storageProvider';
 
 export const MLModel = objectType({
     name: 'Model',
     definition(t) {
-        t.id('modelId')
-        t.string('storageProviderId')
-        t.boolean('isArchived')
-        t.string('modelName')
-        t.string('createdBy') // TODO: resolve actual user object
-        t.string('modifiedBy')
-        t.string('dateCreated')
-        t.string('dateModified')
-        t.string('description')
+        t.id('modelId');
+        t.field('storageProvider', {
+            type: 'StorageProvider',
+            async resolve(root, _args, _ctx) {
+                ObjectionStorageProvider.query().findById(root.providerId).first();
+            },
+        });
+        t.boolean('isArchived');
+        t.string('modelName');
+        t.string('createdBy'); // TODO: resolve actual user object
+        t.string('modifiedBy');
+        t.string('dateCreated');
+        t.string('dateModified');
+        t.string('description');
         // holding off on metadata bc I don't want to deal
         // with JSON serialization/deserialization right now
     },
-})
+});
 
 export class ObjectionMLModel extends Model {
-    id!: string
-    storageProviderId!: string
-    isArchived!: boolean
-    modelName!: string
-    createdBy!: string
-    modifiedBy!: string
-    dateCreated!: string
-    dateModified!: string
-    description!: string
+    id!: string;
+    storageProviderId!: string;
+    isArchived!: boolean;
+    modelName!: string;
+    createdBy!: string;
+    modifiedBy!: string;
+    dateCreated!: string;
+    dateModified!: string;
+    description!: string;
 
-    static tableName = 'registryModels'
+    static tableName = 'registryModels';
     static get idColumn() {
         return 'modelId';
-      }
+    }
 }
