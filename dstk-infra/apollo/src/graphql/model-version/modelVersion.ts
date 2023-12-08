c8e5bbc3dc0fa5a5acc6873016450237324518a7
StephenODea54 <odeastephen1@gmail.com>
2023-12-07 20:44:07 -0500

    Add Registry Model Versions to GraphQL Schema

---
+++ dstk-infra/apollo/src/graphql/model-version/modelVersion.ts
@@ -0,0 +1,38 @@
+import { objectType } from 'nexus';
+import { MLModel, ObjectionMLModel } from '../model/model';
+import { Model } from 'objection';
+
+export const MLModelVersion = objectType({
+    name: 'MLModel',
+    definition(t) {
+        t.id('modelVersionId');
+        t.field('modelId', {
+            type: MLModel,
+            async resolve(root: ObjectionMLModelVersion, _args, _ctx) {
+                ObjectionMLModel.query().findById(root.modelId).first();
+            },
+        });
+        t.boolean('isArchived');
+        t.string('createdBy'); // TODO: Resolve actual user object
+        t.int('numericVersion');
+        t.string('description');
+        // TODO: Custom Scalar Type for JSON Object: t.something('metadata')
+        t.string('dateCreated');
+    },
+});
+
+export class ObjectionMLModelVersion extends Model {
+    id!: string;
+    modelId!: string;
+    isArchived!: boolean;
+    createdBy!: string;
+    numericVersion!: number;
+    description: string;
+    // TODO: metadata: something
+    dateCreated!: string;
+
+    static tableName = 'registryModelVersions';
+    static get idColumn() {
+        return 'modelVersionId';
+    }
+}
