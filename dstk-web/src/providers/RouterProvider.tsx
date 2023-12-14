6278acccabe3617dbc1f7725351e70ce5c794687
Stephen O'Dea <odeastephen1@gmail.com>
2023-12-14 14:53:27 -0500

    Configure frontend niceties (#47)
    * Fix eslint, prettier, husky, and path alias
    
    * Format files

--- dstk-web/src/providers/RouterProvider.tsx
+++ dstk-web/src/providers/RouterProvider.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider as Router } from 'react-router-dom';
 
-import { Dashboard } from '../routes/dashboard';
+import { Dashboard } from '@/routes/dashboard';
 
 // TODO: Will modularize this with protected / public routes whenever we get there
 export const RouterProvider = () => {
