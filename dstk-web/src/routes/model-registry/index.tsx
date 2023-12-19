ac75a761ceab65b6ba9453a078f4d569c955cfce
StephenODea54 <odeastephen1@gmail.com>
2023-12-19 14:02:12 -0500

    Create wireframe for model management ui

--- dstk-web/src/routes/model-registry/index.tsx
+++ dstk-web/src/routes/model-registry/index.tsx
@@ -1,3 +1,81 @@
+import {
+    BreadcrumbItem,
+    Breadcrumbs,
+    Button,
+    StatusIcon,
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeaderCell,
+    TableRow,
+} from '@/components/ui';
+
 export const ModelRegistry = () => {
-    return <div>Hello from the model registry page!</div>;
+    return (
+        <div className='w-full flex flex-col gap-12'>
+            {/* Page Header */}
+            <header className='flex flex-col gap-4'>
+                <div>
+                    <Breadcrumbs>
+                        <BreadcrumbItem href='/dashboard'>Dashboard</BreadcrumbItem>
+                        <BreadcrumbItem href='/models'>Models</BreadcrumbItem>
+                    </Breadcrumbs>
+                </div>
+                <div className='flex items-center justify-between gap-0'>
+                    <h2 className='text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight'>
+                        Model Registry
+                    </h2>
+                    <Button size='lg'>Create</Button>
+                </div>
+            </header>
+            <Table className='whitespace-nowrap overflow-x-scroll'>
+                <TableHead>
+                    <TableRow>
+                        <TableHeaderCell>Name</TableHeaderCell>
+                        <TableHeaderCell>Total Versions</TableHeaderCell>
+                        <TableHeaderCell>Deployments</TableHeaderCell>
+                        <TableHeaderCell>Created By</TableHeaderCell>
+                        <TableHeaderCell>Last Modified</TableHeaderCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    <TableRow>
+                        <TableCell className='font-medium text-gray-900'>
+                            Housing Market Clustering
+                        </TableCell>
+                        <TableCell>5</TableCell>
+                        <TableCell>
+                            <div className='flex flex-col gap-2'>
+                                <div className='flex items-center gap-2'>
+                                    <StatusIcon variant='success' />3 Active
+                                </div>
+                                <div className='flex items-center gap-2'>
+                                    <StatusIcon variant='pending' />1 Pending
+                                </div>
+                                <div className='flex items-center gap-2'>
+                                    <StatusIcon variant='archived' />1 Archived
+                                </div>
+                            </div>
+                        </TableCell>
+                        <TableCell>Steve O</TableCell>
+                        <TableCell>December 12th, 2023</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell className='font-medium text-gray-900'>Employee Churn</TableCell>
+                        <TableCell>3</TableCell>
+                        <TableCell>
+                            <div className='flex flex-col gap-2'>
+                                <div className='flex items-center gap-2'>
+                                    <StatusIcon variant='error' />3 Failed
+                                </div>
+                            </div>
+                        </TableCell>
+                        <TableCell>Michael L</TableCell>
+                        <TableCell>December 10th, 2023</TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+        </div>
+    );
 };
