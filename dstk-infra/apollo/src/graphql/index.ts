fb4302f343355d07fc7fb5eb55d7894f66fc8e6a
Christopher Wetherill <git@tbmh.org>
2023-12-22 14:33:21 -0500

    Adds query to list versions for a model
    Refs #25
    
    Nothing fancy here, pretty much just does what it says on the tin

--- dstk-infra/apollo/src/graphql/index.ts
+++ dstk-infra/apollo/src/graphql/index.ts
@@ -1,10 +1,11 @@
 export * from './model/model.js';
-export * from './model/modelQueries.js';
 export * from './model/modelMutations.js';
+export * from './model/modelQueries.js';
 
 export * from './model-version/modelVersion.js';
 export * from './model-version/modelVersionMutations.js';
+export * from './model-version/modelVersionQueries.js';
 
 export * from './storage-provider/storageProvider.js';
-export * from './storage-provider/storageProviderQueries.js';
 export * from './storage-provider/storageProviderMutations.js';
+export * from './storage-provider/storageProviderQueries.js';
