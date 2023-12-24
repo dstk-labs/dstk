4e0a043642dddad7d1e569a04503d81bc438f608
Christopher Wetherill <git@tbmh.org>
2023-12-24 16:03:05 -0500

    presigned URL port

--- dstk-infra/apollo/src/graphql/model-version/modelVersionMutations.ts
+++ dstk-infra/apollo/src/graphql/model-version/modelVersionMutations.ts
@@ -11,11 +11,53 @@ import {
 } from '../../utils/s3-api.js';
 import { raw } from 'objection';
 import { builder } from '../../builder.js';
+import { resolve } from 'path';
 
 export const ModelVersionInputType = builder.inputType('ModelVersionInputType', {
     fields: (t) => ({
         modelId: t.string({ required: true }),
         description: t.string(),
+    }),
+});
+
+export const CompletedPartInputType = builder.inputType('CompletedPartInputType', {
+    fields: (t) => ({
+        ETag: t.string({ required: true }),
+        PartNumber: t.int({ required: true }),
+    }),
+});
+
+export const PartsInputType = builder.inputType('PartsInputType', {
+    fields: (t) => ({
+        Parts: t.field({
+            required: true,
+            type: [CompletedPartInputType],
+        }),
+    }),
+});
+
+export const PresignMethod = builder.enumType('PresignMethod', {
+    values: [
+        'createMultipartUpload',
+        'uploadPart',
+        'finalizeMultipartUpload',
+        'abortMultipartUpload',
+    ] as const,
+});
+
+export const PresignedURLInputType = builder.inputType('PresignedURLInputType', {
+    fields: (t) => ({
+        modelVersionId: t.string({ required: true }),
+        method: t.field({
+            required: true,
+            type: PresignMethod,
+        }),
+        filename: t.string(),
+        uploadId: t.string(),
+        partNumber: t.int(),
+        multipartUpload: t.field({
+            type: PartsInputType,
+        }),
     }),
 });
 
@@ -109,134 +151,99 @@ builder.mutationFields((t) => ({
             return results;
         },
     }),
+    presignURL: t.field({
+        type: PresignedURL,
+        args: {
+            data: t.arg({ type: PresignedURLInputType, required: true }),
+        },
+        async resolve(root, args, ctx) {
+            const modelStorageProvider = (await ObjectionMLModelVersion.relatedQuery(
+                'storageProvider',
+            )
+                .for(args.data.modelVersionId)
+                .first()) as ObjectionStorageProvider;
+            if (modelStorageProvider.isArchived === true) {
+                throw new RegistryOperationError({ name: 'ARCHIVED_STORAGE_ERROR' });
+            }
+
+            const parentModel = (await ObjectionMLModelVersion.relatedQuery('model')
+                .for(args.data.modelVersionId)
+                .first()) as ObjectionMLModel;
+            if (parentModel.isArchived === true) {
+                throw new RegistryOperationError({ name: 'ARCHIVED_MODEL_ERROR' });
+            }
+
+            const modelVersion = (await ObjectionMLModelVersion.query()
+                .findById(args.data.modelVersionId)
+                .first()) as ObjectionMLModelVersion;
+            if (modelVersion.isArchived === true) {
+                throw new RegistryOperationError({ name: 'ARCHIVED_MODEL_VERSION_ERROR' });
+            } else if (modelVersion.isFinalized === true) {
+                throw new RegistryOperationError({ name: 'PUBLISHED_MODEL_VERSION_ERROR' });
+            }
+
+            let mpu_url;
+            const key = `${modelVersion.s3Prefix}/${args.data.filename}`;
+
+            if (args.data.method === 'createMultipartUpload') {
+                const result = await CreateMultipartUpload(modelStorageProvider, key);
+
+                mpu_url = {
+                    uploadId: result.UploadId,
+                    key: result.Key,
+                };
+            }
+
+            if (!args.data.uploadId) {
+                throw new RegistryOperationError({ name: 'MISSING_UPLOAD_ID_ERROR' });
+            }
+            if (!args.data.partNumber) {
+                throw new RegistryOperationError({ name: 'MISSING_PART_NUM_ERROR' });
+            }
+            if (args.data.method === 'uploadPart') {
+                const result = await CreatePresignedURLForPart(
+                    modelStorageProvider,
+                    key,
+                    args.data.uploadId,
+                    args.data.partNumber,
+                );
+
+                mpu_url = {
+                    uploadId: args.data.uploadId,
+                    key: key,
+                    partNumber: args.data.partNumber,
+                    url: result,
+                };
+            } else if (args.data.method === 'abortMultipartUpload') {
+                const result = await AbortMultipartUpload(
+                    modelStorageProvider,
+                    key,
+                    args.data.uploadId,
+                );
+            }
+
+            if (!args.data.multipartUpload) {
+                throw new RegistryOperationError({ name: 'MULTIPART_FINALIZATION_ERROR' });
+            }
+            if (args.data.method === 'finalizeMultipartUpload') {
+                const result = await FinalizeMultipartUpload(
+                    modelStorageProvider,
+                    key,
+                    args.data.uploadId,
+                    args.data.multipartUpload,
+                );
+                mpu_url = {
+                    uploadId: args.data.uploadId,
+                    key: key,
+                    ETag: result.ETag,
+                    url: result.Location,
+                };
+                mpu_url = {};
+            } else {
+                mpu_url = {};
+            }
+
+            return mpu_url as typeof PresignedURL.$inferType;
+        },
+    }),
 }));
-
-// export const CompletedPartInputType = inputObjectType({
-//     name: 'CompletedPartUploadInput',
-//     definition(t) {
-//         t.nonNull.string('ETag');
-//         t.nonNull.int('PartNumber');
-//     },
-// });
-
-// export const PartsInputType = inputObjectType({
-//     name: 'PartsInputType',
-//     definition(t) {
-//         t.list.field(
-//             'Parts', // This is capitalized to conform to the AWS SDK
-//             { type: CompletedPartInputType },
-//         );
-//     },
-// });
-
-// const PresignMethod = enumType({
-//     name: 'method',
-//     members: [
-//         'createMultipartUpload',
-//         'uploadPart',
-//         'finalizeMultipartUpload',
-//         'abortMultipartUpload',
-//     ],
-// });
-
-// export const PresignedURLInputType = inputObjectType({
-//     name: 'PresignedURLInput',
-//     definition(t) {
-//         t.nonNull.string('modelVersionId');
-//         t.nonNull.field('method', {
-//             type: PresignMethod,
-//         });
-//         t.nonNull.string('filename');
-//         t.string('uploadId');
-//         t.int('partNumber');
-//         t.field('multipartUpload', {
-//             type: PartsInputType,
-//         });
-//     },
-// });
-
-// export const PresignURLForModelVersion = extendType({
-//     type: 'Mutation',
-//     definition(t) {
-//         t.field('presignURL', {
-//             type: PresignedURL,
-//             args: { data: PresignedURLInputType },
-//             async resolve(root, args, ctx) {
-//                 const modelStorageProvider = (await ObjectionMLModelVersion.relatedQuery(
-//                     'storageProvider',
-//                 )
-//                     .for(args.data.modelVersionId)
-//                     .first()) as ObjectionStorageProvider;
-//                 if (modelStorageProvider.isArchived === true) {
-//                     throw new RegistryOperationError({ name: 'ARCHIVED_STORAGE_ERROR' });
-//                 }
-
-//                 const parentModel = (await ObjectionMLModelVersion.relatedQuery('model')
-//                     .for(args.data.modelVersionId)
-//                     .first()) as ObjectionMLModel;
-//                 if (parentModel.isArchived === true) {
-//                     throw new RegistryOperationError({ name: 'ARCHIVED_MODEL_ERROR' });
-//                 }
-
-//                 const modelVersion = await ObjectionMLModelVersion.query()
-//                     .where('modelVersionId', args.data.modelVersionId)
-//                     .first();
-//                 if (modelVersion.isArchived === true) {
-//                     throw new RegistryOperationError({ name: 'ARCHIVED_MODEL_VERSION_ERROR' });
-//                 } else if (modelVersion.isFinalized === true) {
-//                     throw new RegistryOperationError({ name: 'PUBLISHED_MODEL_VERSION_ERROR' });
-//                 }
-
-//                 let mpu_url;
-//                 const key = `${modelVersion.s3Prefix}/${args.data.filename}`;
-
-//                 if (args.data.method === 'createMultipartUpload') {
-//                     const result = await CreateMultipartUpload(modelStorageProvider, key);
-
-//                     mpu_url = {
-//                         uploadId: result.UploadId,
-//                         key: result.Key,
-//                     };
-//                 } else if (args.data.method === 'uploadPart') {
-//                     const result = await CreatePresignedURLForPart(
-//                         modelStorageProvider,
-//                         key,
-//                         args.data.uploadId,
-//                         args.data.partNumber,
-//                     );
-
-//                     mpu_url = {
-//                         uploadId: args.data.uploadId,
-//                         key: key,
-//                         partNumber: args.data.partNumber,
-//                         url: result,
-//                     };
-//                 } else if (args.data.method === 'finalizeMultipartUpload') {
-//                     const result = await FinalizeMultipartUpload(
-//                         modelStorageProvider,
-//                         key,
-//                         args.data.uploadId,
-//                         args.data.multipartUpload,
-//                     );
-//                     mpu_url = {
-//                         uploadId: args.data.uploadId,
-//                         key: key,
-//                         ETag: result.ETag,
-//                         url: result.Location,
-//                     };
-//                 } else if (args.data.method === 'abortMultipartUpload') {
-//                     const result = await AbortMultipartUpload(
-//                         modelStorageProvider,
-//                         key,
-//                         args.data.uploadId,
-//                     );
-//                     mpu_url = {};
-//                 } else {
-//                     mpu_url = {};
-//                 }
-
-//                 return mpu_url;
-//             },
-//         });
-//     },
-// });
--- dstk-infra/apollo/src/utils/errors.ts
+++ dstk-infra/apollo/src/utils/errors.ts
@@ -2,13 +2,20 @@ type RegistryErrorName =
     | 'ARCHIVED_STORAGE_ERROR'
     | 'ARCHIVED_MODEL_ERROR'
     | 'ARCHIVED_MODEL_VERSION_ERROR'
-    | 'PUBLISHED_MODEL_VERSION_ERROR';
+    | 'PUBLISHED_MODEL_VERSION_ERROR'
+    | 'MISSING_UPLOAD_ID_ERROR'
+    | 'MISSING_PART_NUM_ERROR'
+    | 'MULTIPART_FINALIZATION_ERROR';
 
 const RegistryErrorMessages = {
     ARCHIVED_STORAGE_ERROR: 'New model versions cannot be added to an archived storage provider',
     ARCHIVED_MODEL_ERROR: 'New model versions cannot be added to archived models',
     ARCHIVED_MODEL_VERSION_ERROR: 'An archived model version cannot be modified',
     PUBLISHED_MODEL_VERSION_ERROR: 'Published model versions cannot be modified',
+    MISSING_UPLOAD_ID_ERROR: 'An Upload ID must be supplied for this operation',
+    MISSING_PART_NUM_ERROR: 'A Part Number must be supuplied for this operation',
+    MULTIPART_FINALIZATION_ERROR:
+        'Uploaded parts and their ETags must be supplied to finalize a MPU',
 };
 
 export class RegistryOperationError extends Error {
