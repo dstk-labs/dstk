ea7654574b4430ee2369b1ee05753b16daa8caa2
StephenODea54 <odeastephen1@gmail.com>
2023-12-26 13:51:19 -0500

    Remove UUID extension in currentModelVersion patch

--- dstk-infra/postgres/patches/20231226_current-model-version-key.sql
+++ dstk-infra/postgres/patches/20231226_current-model-version-key.sql
@@ -1,7 +1,5 @@
 \connect dstk;
 
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
 ALTER TABLE registry.models
 ADD COLUMN current_model_version_id UUID
 CONSTRAINT current_model_version_fk REFERENCES registry.model_versions(model_version_id);
