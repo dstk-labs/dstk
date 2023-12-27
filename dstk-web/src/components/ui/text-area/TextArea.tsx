cbba806cea3d76b3e03f4b9074226215c8d65fbe
StephenODea54 <odeastephen1@gmail.com>
2023-12-27 14:10:17 -0500

    Add ref for TextArea component

--- dstk-web/src/components/ui/text-area/TextArea.tsx
+++ dstk-web/src/components/ui/text-area/TextArea.tsx
@@ -1,10 +1,15 @@
+import { forwardRef } from 'react';
+
 type TextAreaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>;
 
-export const TextArea = ({ ...props }: TextAreaProps) => {
+export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(({ ...props }, ref) => {
     return (
         <textarea
             className='block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+            ref={ref}
             {...props}
         />
     );
-};
+});
+
+TextArea.displayName = 'TextArea';
