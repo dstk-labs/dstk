8118bbe6126be4cea05b9f9bfcc47379280650e4
StephenODea54 <odeastephen1@gmail.com>
2023-12-28 09:53:04 -0500

    Add modelName arg to ListMLModels Query

--- dstk-web/src/routes/model-registry/api/listModels.ts
+++ dstk-web/src/routes/model-registry/api/listModels.ts
@@ -3,12 +3,12 @@ import { gql, useQuery, type TypedDocumentNode } from '@apollo/client';
 import type { MLModelList } from '@/types/MLModel';
 
 const LIST_MODELS: TypedDocumentNode<MLModelList> = gql`
-    query ListMLModels {
-        listMLModels {
-            createdBy
+    query ListMLModels($modelName: String) {
+        listMLModels(modelName: $modelName) {
             modelName
             modelId
-            dateModified
+            createdBy
+            dateCreated
             currentModelVersion {
                 modelVersionId
                 numericVersion
