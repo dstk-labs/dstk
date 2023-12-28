8e0c81a36df2edfdfa8588c0ed94af4a61e67ee6
StephenODea54 <odeastephen1@gmail.com>
2023-12-28 11:16:34 -0500

    Add Divider Component

---
+++ dstk-web/src/components/ui/divider/Divider.tsx
@@ -0,0 +1,7 @@
+export const Divider = () => {
+    return (
+        <div className='flex items-center'>
+            <div className='w-full border-t border-gray-300' />
+        </div>
+    );
+};
--- dstk-web/src/components/ui/index.ts
+++ dstk-web/src/components/ui/index.ts
@@ -1,6 +1,7 @@
 export * from './badge';
 export * from './breadcrumb';
 export * from './button';
+export * from './divider';
 export * from './dropdown';
 export * from './input';
 export * from './notification';
