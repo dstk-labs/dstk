dstk-labs/dstk

commit e6ffeb2065d998366f8cca67aaf6816543116b6d

Add List ML Models By Name Query (#88)
* Add list ML models by name query

* Add model name as optional arg to listMLModels

apollo/src/graphql/model/modelQueries.ts
@@ -4,8 +4,13 @@ import { builder } from '../../builder.js';
 builder.queryFields((t) => ({
     listMLModels: t.field({
         type: [MLModel],
-        async resolve(root, args, ctx) {
-            const mlModel = await ObjectionMLModel.query().orderBy('dateCreated');
+        args: {
+            modelName: t.arg.string(),
+        },
+        async resolve(_root, args, _ctx) {
+            const mlModel = await ObjectionMLModel.query()
+                .where('modelName', 'LIKE', `${args.modelName || ''}%`)
+                .orderBy('dateCreated');
             return mlModel;
         },
     }),

commit 06ff6be86e611c0f36e57d7a80734b171484beeb

Fix listModels regression in e6ffeb2

apollo/src/graphql/model/modelQueries.ts
@@ -8,9 +8,11 @@ builder.queryFields((t) => ({
             modelName: t.arg.string(),
         },
         async resolve(_root, args, _ctx) {
-            const mlModel = await ObjectionMLModel.query()
-                .where('modelName', 'LIKE', `${args.modelName || ''}%`)
-                .orderBy('dateCreated');
+            const query = ObjectionMLModel.query();
+            if (args.modelName) {
+                query.where('modelName', 'ILIKE', `%${args.modelName}%`);
+            }
+            const mlModel = await query.orderBy('dateCreated');
             return mlModel;
         },
     }),