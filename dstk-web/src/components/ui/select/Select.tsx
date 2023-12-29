292b87979d68d44ab95c1bacf730c18f1b414a67
StephenODea54 <odeastephen1@gmail.com>
2023-12-28 22:56:14 -0500

    Swap out Select Component for native select element

--- dstk-web/src/components/ui/select/Select.tsx
+++ dstk-web/src/components/ui/select/Select.tsx
@@ -1,40 +1,19 @@
-import React, { Fragment } from 'react';
-import { Listbox, Transition } from '@headlessui/react';
-import { ChevronUpDownIcon } from '@heroicons/react/20/solid';
+import { cn } from '@/lib/cn';
 
 export type SelectProps = {
     children: React.ReactNode;
-    onChange: React.Dispatch<React.SetStateAction<string>>;
-    value: string;
-};
+} & React.SelectHTMLAttributes<HTMLSelectElement>;
 
-export const Select = ({ children, onChange, value }: SelectProps) => {
+export const Select = ({ children, className, ...props }: SelectProps) => {
     return (
-        <Listbox value={value} onChange={onChange}>
-            {({ open }) => (
-                <>
-                    <div className='relative'>
-                        <Listbox.Button className='relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6'>
-                            <span className='block truncate'>{value}</span>
-                            <span className='pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2'>
-                                <ChevronUpDownIcon className='h-5 w-5 text-gray-400' />
-                            </span>
-                        </Listbox.Button>
-
-                        <Transition
-                            show={open}
-                            as={Fragment}
-                            leave='transition ease-in duration-100'
-                            leaveFrom='opacity-100'
-                            leaveTo='opacity-0'
-                        >
-                            <Listbox.Options className='absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'>
-                                {children}
-                            </Listbox.Options>
-                        </Transition>
-                    </div>
-                </>
+        <select
+            className={cn(
+                'block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6',
+                className,
             )}
-        </Listbox>
+            {...props}
+        >
+            {children}
+        </select>
     );
 };
