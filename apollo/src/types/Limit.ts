b4eef8bb294d63953447eefd63c0873a2e43bf57
StephenODea54 <odeastephen1@gmail.com>
2023-12-30 18:25:56 -0500

    Use limit enum to define type and array

--- apollo/src/types/Limit.ts
+++ apollo/src/types/Limit.ts
@@ -1,4 +1,10 @@
-export const LIMITS = [10, 25, 50] as const;
+enum LimitEnum {
+    _10 = 10,
+    _25 = 25,
+    _50 = 50,
+}
 
-type LimitTuple = typeof LIMITS;
-export type Limit = LimitTuple[number];
+export type Limit = `${LimitEnum}` extends `${infer T extends number}` ? T : never;
+export const LIMITS = Object.keys(LimitEnum)
+    .filter((limit) => !isNaN(Number(limit)))
+    .map((limit) => parseInt(limit));
