a6b729f71fc902abb64833b9c5677eb0f84c5496
Christopher Wetherill <1910269+wetherc@users.noreply.github.com>
2024-01-04 13:54:19 -0500

    Add basic support for Teams (#105)
    Refs #75.
    
    This adds basic support for teams and team creation
    including backing database tables and a `createTeam`
    mutation.
    
    Note that for permissions, I'm using edges. Edges are
    a generic way of storing a relationship between two
    objects, and an edge is defined by an origin ID, a
    destination ID, and an edge type which describes the
    relationship. Every edge is inherently bidirectional,
    although for simplicity I am choosing to not write an
    inverse edge since this can effectively be achieved
    already by just inverting the direction of a join.

---
+++ apollo/src/graphql/user/team.ts
@@ -0,0 +1,153 @@
+import { builder } from '../../builder.js';
+import { Model, AnyQueryBuilder } from 'objection';
+import { User, ObjectionUser } from '../user/user.js';
+import { ObjectionEdge } from '../misc/edges.js';
+import { deepStrictEqual } from 'assert';
+
+export const Team = builder.objectRef<ObjectionTeam>('Team');
+
+builder.objectType(Team, {
+    fields: (t) => ({
+        teamId: t.field({
+            type: 'ID',
+            resolve(root: ObjectionTeam, _args, _ctx) {
+                return root.$id();
+            },
+        }),
+        name: t.exposeString('name'),
+        description: t.exposeString('description'),
+        dateCreated: t.exposeString('dateCreated'),
+        dateModified: t.exposeString('dateModified'),
+        createdBy: t.field({
+            type: User,
+            async resolve(root: ObjectionTeam, _args, _ctx) {
+                const user = (await root.$relatedQuery('getCreatedBy')
+                    .for(root.$id())
+                    .first()) as ObjectionUser;
+                return user;
+            },
+        }),
+        modifiedBy: t.field({
+            type: User,
+            async resolve(root: ObjectionTeam, _args, _ctx) {
+                const user = (await root.$relatedQuery('getModifiedBy')
+                    .for(root.$id())
+                    .first()) as ObjectionUser;
+                return user;
+            },
+        }),
+        owner: t.field({
+            type: User,
+            async resolve(root: ObjectionTeam, _args, _ctx) {
+                const user = (await root.$relatedQuery('getOwnedBy')
+                    .for(root.$id())
+                    .first()) as ObjectionUser;
+                return user;
+            },
+        }),
+    }),
+});
+
+export class ObjectionTeam extends Model {
+    id!: number;
+    teamId!: string;
+    name!: string;
+    description!: string;
+    isArchived!: boolean;
+    dateCreated!: string;
+    dateModified!: string;
+    createdById!: string;
+    modifiedById!: string;
+    ownerId!: string;
+
+    createdBy!: ObjectionUser;
+    modifiedBy!: ObjectionUser;
+    ownedBy!: ObjectionUser;
+
+    static tableName = 'dstkUser.teams';
+    static get idColumn() {
+        return 'teamId';
+    }
+
+    static get modifiers() {
+        return {
+            filterByEdgeType(_builder: AnyQueryBuilder, edgeType: number) {
+                _builder.withGraphJoined('teamMembers')
+                    .where('dstkUser.teamEdges.edgeType', edgeType);
+            }
+        }
+    }
+
+    static relationMappings = () => ({
+        getCreatedBy: {
+            relation: Model.HasOneRelation,
+            modelClass: ObjectionUser,
+            join: {
+                from: 'dstkUser.teams.createdById',
+                to: 'dstkUser.user.userId',
+            },
+        },
+        getModifiedBy: {
+            relation: Model.HasOneRelation,
+            modelClass: ObjectionUser,
+            join: {
+                from: 'dstkUser.teams.modifiedById',
+                to: 'dstkUser.user.userId',
+            },
+        },
+        getOwnedBy: {
+            relation: Model.HasOneRelation,
+            modelClass: ObjectionUser,
+            join: {
+                from: 'dstkUser.teams.ownerId',
+                to: 'dstkUser.user.userId',
+            },
+        },
+        teamMembers: {
+            relation: Model.HasManyRelation,
+            modelClass: ObjectionUser,
+            join: {
+                from: 'dstkUser.teams.teamId',
+                through: {
+                    from: 'dstkUser.team_edges.teamId',
+                    to: 'dstkUser.team_edges.userId'
+                },
+                to: 'dstkUser.user.userId',
+            },
+        }
+    });
+}
+
+export class ObjectionTeamEdge extends Model {
+    teamId!: string;
+    edgeType!: number;
+    userId!: string;
+
+    static tableName = 'dstkUser.teamEdges';
+    static relationMappings = () => ({
+        user: {
+            relation: Model.HasOneRelation,
+            modelClass: ObjectionUser,
+            join: {
+                from: 'dstkUser.teamEdges.userId',
+                to: 'dstkUser.user.userId'
+            },
+        },
+        team: {
+            relation: Model.HasOneRelation,
+            modelClass: ObjectionTeam,
+            join: {
+                from: 'dstkUser.teamEdges.teamId',
+                to: 'dstkUser.team.teamId'
+            },
+        },
+        edgeTypeMapping: {
+            relation: Model.HasOneRelation,
+            modelClass: ObjectionEdge,
+            join: {
+                from: 'dstkUser.teamEdges.edgeType',
+                to: 'dstkMetadata.edgeRelations.id'
+            },
+        },
+    });
+}
