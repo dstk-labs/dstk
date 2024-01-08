f30cbc366764ff70c10390acc90b60d2a5f9a2cf
Christopher Wetherill <1910269+wetherc@users.noreply.github.com>
2024-01-08 13:12:34 -0500

    Add team and team member queries (#112)
    This adds two queries to return detail about teams
    a user is a member of, and to return membership detail
    for those teams

---
+++ apollo/src/graphql/user/teamQueries.ts
@@ -0,0 +1,49 @@
+import { Team, ObjectionTeam, ObjectionTeamEdge } from './team.js';
+import { ObjectionUser, User } from './user.js';
+import { builder } from '../../builder.js';
+
+builder.queryFields((t) =>({
+    listTeams: t.field({
+        type: [Team],
+        authScopes: {
+            loggedIn: true,
+        },
+        args: {
+            teamId: t.arg.string(),
+        },
+        async resolve(root, args, ctx) {
+            const userTeamEdges = ObjectionTeamEdge.query()
+                .where('userId', ctx.user.$id())
+            if (args.teamId) {
+                userTeamEdges.where('teamId', args.teamId);
+            }
+
+            const userTeams = await ObjectionTeamEdge.relatedQuery('team')
+                .for(userTeamEdges) as [ObjectionTeam];
+            return userTeams;
+        }
+    }),
+    listTeamMembers: t.field({
+        type: [User],
+        authScopes: {
+            loggedIn: true,
+        },
+        args: {
+            teamId: t.arg.string({ required: true }),
+        },
+        async resolve(root, args, ctx) {
+            const userCanView = await ObjectionTeamEdge.query()
+                .where({
+                    userId: ctx.user.$id(),
+                    teamId: args.teamId
+                }).first();
+            if (userCanView === undefined) {
+                return [];
+            }
+
+            const teamMembers = await ObjectionTeam.relatedQuery('teamMembers')
+                .for(args.teamId) as [ObjectionUser];
+            return teamMembers;
+        }
+    }),
+}));
