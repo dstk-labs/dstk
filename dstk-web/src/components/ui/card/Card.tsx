4c8978ef95c93cce5e644ef40dd827ca3f6fdd7d
Stephen O'Dea <odeastephen1@gmail.com>
2024-08-19 10:07:36 -0400

    restyle card component (#54)

--- dstk-web/src/components/ui/card/Card.tsx
+++ dstk-web/src/components/ui/card/Card.tsx
@@ -1,13 +1,29 @@
-import { cn } from '@/lib/cn';
+import { forwardRef } from 'react';
+import { Slot } from '@radix-ui/react-slot';
+import { cn } from '@/lib';
 
-export type CardProps = {
-    children: React.ReactNode;
-} & React.AllHTMLAttributes<HTMLDivElement>;
-
-export const Card = ({ children, className, ...props }: CardProps) => {
-    return (
-        <div className={cn('overflow-hidden rounded-lg bg-white shadow', className)} {...props}>
-            {children}
-        </div>
-    );
+export type CardProps = React.ComponentPropsWithoutRef<'div'> & {
+    asChild?: boolean;
 };
+
+export const Card = forwardRef<HTMLDivElement, CardProps>(
+    ({ className, asChild, ...props }, forwardedRef) => {
+        const Component = asChild ? Slot : 'div';
+        return (
+            <Component
+                ref={forwardedRef}
+                className={cn(
+                    // base
+                    'relative w-full rounded-lg border p-6 text-left shadow-sm',
+                    // background color
+                    'bg-white dark:bg-[#090E1A]',
+                    // border color
+                    'border-gray-200 dark:border-gray-900',
+                    className,
+                )}
+                {...props}
+            />
+        );
+    },
+);
+Card.displayName = 'Card';
