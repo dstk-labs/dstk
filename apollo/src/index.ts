8b6dd7dcb06d98f1440d19fba7e7fcae8c844036
StephenODea54 <odeastephen1@gmail.com>
2025-03-30 23:28:11 -0400

    add better auth and apollo middleware to index file

--- apollo/src/index.ts
+++ apollo/src/index.ts
@@ -1,73 +1,89 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { ApolloServer, HeaderMap } from '@apollo/server';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { fromNodeHeaders, toNodeHandler } from 'better-auth/node';
+import express from 'express';
+import type { Request, Response } from 'express';
+import http from 'http';
 import { Model } from 'objection';
 import Knex from 'knex';
 import { knexConfig } from './knexfile.js';
 import { schema } from './graphql/index.js';
-import { JWTValidator } from './utils/jwt.js';
-import { IncomingMessage, ServerResponse } from 'http';
-import { ObjectionUser } from './graphql/index.js';
+import { auth } from './utils/auth.js';
+import { GraphQLError } from 'graphql';
 
-const JWT = new JWTValidator();
 const knex = Knex(knexConfig.development);
 Model.knex(knex);
 
-const createContext = async ({ res, req }: { res: ServerResponse; req: IncomingMessage }) => {
-    // simple auth check on every request
-    const auth = (req.headers && req.headers.authorization) || '';
-    if (auth.startsWith('Bearer ')) {
-        const token = auth.substring(7, auth.length);
-        try {
-            const accessToken = await JWT.verifySession(token, 'access');
+const app = express();
+const httpServer = http.createServer(app);
 
-            const user = (await ObjectionUser.query()
-                .findById(accessToken?.sub || '')
-                .where({ isDisabled: false })) as ObjectionUser;
+app.all('/api/auth/*splat', toNodeHandler(auth));
 
-            res.setHeader('Authorization', JWT.encodeSession({ sub: user.$id() }, 'access'));
+const apollo = new ApolloServer({
+    schema,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
 
-            return { user: user };
-        } catch (err) {
-            // throw new GraphQLError('Authentication token is invalid', {
-            //     extensions: {
-            //       code: 'UNAUTHENTICATED',
-            //       http: { status: 401 },
-            //     },
-            // });
-            return { user: ObjectionUser };
-        }
-    } else if (auth.startsWith('Basic ')) {
-        const token = auth.substring(6, auth.length);
-        try {
-            // const verifiedToken = JWT.verifySession(token) as JwtPayload;
-            return { user: ObjectionUser };
-        } catch (err) {
-            // throw new GraphQLError('Authentication token is invalid', {
-            //     extensions: {
-            //       code: 'UNAUTHENTICATED',
-            //       http: { status: 401 },
-            //     },
-            // });
-            return { user: ObjectionUser };
-        }
-    } else {
-        // throw new GraphQLError('User is not authenticated', {
-        //     extensions: {
-        //       code: 'UNAUTHENTICATED',
-        //       http: { status: 401 },
-        //     },
-        // });
-        return { user: ObjectionUser };
+const createContext = async ({ req }: { req: Request }) => {
+    const session = await auth.api.getSession({
+        headers: fromNodeHeaders(req.headers),
+    });
+
+    if (!session) {
+        throw new GraphQLError('Authentication token is invalid', {
+            extensions: {
+                code: 'UNAUTHENTICATED',
+                http: { status: 401 },
+            },
+        });
     }
+
+    return {
+        user: session.user,
+    };
 };
 
-const server = new ApolloServer({
-    schema,
+app.use('/graphql', express.json(), async ({ req, res }: { req: Request; res: Response }) => {
+    try {
+        const httpGraphQLResponse = await apollo.executeHTTPGraphQLRequest({
+            httpGraphQLRequest: {
+                body: req.method === 'POST' ? await req.body : undefined,
+                headers: new HeaderMap(
+                    Object.entries(req.headers).map(([key, value]) => [
+                        key,
+                        Array.isArray(value) ? value.join(', ') : value || '',
+                    ]),
+                ),
+                method: req.method,
+                search: new URL(req.url).search,
+            },
+            context: async () => createContext({ req }), // getContext is my own graphql context factory
+        });
+
+        const { headers, body, status } = httpGraphQLResponse;
+
+        for (const [headerKey, headerValue] of headers) {
+            res.setHeader(headerKey, headerValue);
+        }
+
+        res.status(status ?? 200);
+
+        if (body.kind === 'complete') {
+            res.send(body.string);
+        }
+
+        // TOOD: Might not need this, but leaving to be safe
+        if (body.kind === 'chunked') {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            for await (const chunk of body.asyncIterator) {
+                res.write(chunk);
+            }
+            res.end();
+        }
+    } catch (error) {
+        res.status(500).json({ error });
+    }
 });
 
-const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
-    context: createContext,
-});
-
-console.log(`🚀  Server ready at: ${url}`);
+await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
+console.log(`🚀 Server ready at ${httpServer.address}`);
