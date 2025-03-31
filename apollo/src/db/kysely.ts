7874c923ab72d71c23b0d23a25e0035243095a34
StephenODea54 <odeastephen1@gmail.com>
2025-03-30 23:26:30 -0400

    add pool export in db file

--- apollo/src/db/kysely.ts
+++ apollo/src/db/kysely.ts
@@ -5,12 +5,12 @@ import { Kysely, PostgresDialect } from 'kysely';
 import pg from 'pg';
 const { Pool } = pg;
 
-const dialect = new PostgresDialect({
-    pool: new Pool({
-        connectionString: process.env.DATABASE_URL,
-        max: 10,
-    }),
+export const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    max: 10,
 });
+
+const dialect = new PostgresDialect({ pool });
 
 export const db = new Kysely<DB>({
     dialect,
