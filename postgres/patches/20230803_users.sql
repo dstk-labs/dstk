d3236e23ef3413371ebca6bf4f0505d6aa09b831
StephenODea54 <odeastephen1@gmail.com>
2025-05-01 22:56:26 -0400

    modify better auth patches; remove previous user password patch

--- postgres/patches/20230803_users.sql
+++ postgres/patches/20230803_users.sql
@@ -3,52 +3,49 @@
 CREATE SCHEMA dstk_user;
 
 CREATE TABLE dstk_user.user (
-    id                SERIAL      NOT NULL PRIMARY KEY,
-    user_id           VARCHAR(16) NOT NULL DEFAULT generate_monogram('USR') UNIQUE,
-    is_admin          BOOLEAN     NOT NULL DEFAULT FALSE,
-    is_approved       BOOLEAN     NOT NULL DEFAULT FALSE,
-    is_disabled       BOOLEAN     NOT NULL DEFAULT FALSE,
-    is_email_verified BOOLEAN     NOT NULL DEFAULT FALSE,
-    is_mfa_enrolled   BOOLEAN     NOT NULL DEFAULT FALSE,
-    real_name         TEXT        NOT NULL,
-    user_name         TEXT        NOT NULL UNIQUE,
-    date_created      TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    date_modified     TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+    id                 TEXT        NOT NULL PRIMARY KEY,
+    user_id            VARCHAR(16) NOT NULL DEFAULT generate_monogram('USR') UNIQUE,
+    real_name          TEXT        NOT NULL,
+    email              TEXT        NOT NULL,
+    is_email_verified  BOOLEAN     NOT NULL DEFAULT FALSE,
+    is_mfa_enrolled    BOOLEAN     NOT NULL DEFAULT FALSE,
+    image              TEXT,
+    user_name          TEXT        NOT NULL,
+    date_created       TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    date_modified      TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE dstk_user.email (
-    id                SERIAL       NOT NULL PRIMARY KEY,
-    email_id          UUID         NOT NULL DEFAULT uuid_generate_v4() UNIQUE,
-    user_id           VARCHAR(16)  NOT NULL REFERENCES dstk_user.user(user_id),
-    email_address     VARCHAR(128) NOT NULL,
-    is_verified       BOOLEAN      NOT NULL DEFAULT FALSE,
-    is_primary        BOOLEAN      NOT NULL,
-    verification_code VARCHAR(64),
-    date_created      TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    date_modified     TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+CREATE TABLE dstk_user.sessions (
+     id                     TEXT      NOT NULL PRIMARY KEY,
+     user_id                TEXT      NOT NULL REFERENCES dstk_user.user(id),
+     token                  TEXT      NOT NULL,
+     ip_address             TEXT,
+     user_agent             TEXT,
+     expires_at             TIMESTAMP NOT NULL,
+     date_created           TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+     date_modified          TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE dstk_user.refresh_token (
-    id             BIGSERIAL   NOT NULL PRIMARY KEY,
-    session_id     UUID        NOT NULL DEFAULT uuid_generate_v4() UNIQUE,
-    session_family UUID        NOT NULL DEFAULT uuid_generate_v4(),
-    user_id        VARCHAR(16) NOT NULL REFERENCES dstk_user.user(user_id),
-    is_partial     BOOLEAN     NOT NULL DEFAULT TRUE,
-    is_revoked     BOOLEAN     NOT NULL DEFAULT FALSE,
-    session_iat    TIMESTAMP WITH TIME ZONE NOT NULL,
-    sesstion_exp   TIMESTAMP WITH TIME ZONE NOT NULL
+CREATE TABLE dstk_user.accounts (
+     id                      TEXT       NOT NULL PRIMARY KEY,
+     account_id              TEXT       NOT NULL,
+     provider_id             TEXT       NOT NULL,
+     user_id                 TEXT       NOT NULL REFERENCES dstk_user.user(id),
+     access_token            TEXT,
+     refresh_token           TEXT,
+     id_token                TEXT,
+     access_token_expires_at TIMESTAMP,
+     scope                   TEXT,
+     password                TEXT,
+     date_created            TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+     date_modified           TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE dstk_user.log (
-    id            BIGSERIAL   NOT NULL PRIMARY KEY,
-    log_id        UUID        NOT NULL DEFAULT uuid_generate_v4() UNIQUE,
-    user_id       VARCHAR(16) NOT NULL REFERENCES dstk_user.user(user_id),
-    actor_id      VARCHAR(16) NOT NULL REFERENCES dstk_user.user(user_id),
-    old_value     TEXT        NOT NULL,
-    new_value     TEXT        NOT NULL,
-    remote_addr   INET        NOT NULL,
-    details       TEXT        NOT NULL,
-    user_action   TEXT        NOT NULL,
-    date_created  TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    date_modified TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+CREATE TABLE dstk_user.verifications (
+     id              TEXT      NOT NULL PRIMARY KEY,
+     identifier      TEXT      NOT NULL,
+     value           TEXT      NOT NULL,
+     expires_at      TIMESTAMP NOT NULL,
+     date_created    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+     date_modified   TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
