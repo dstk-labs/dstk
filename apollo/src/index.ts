37c3ed5ae5ad53af8413c4166ed90abceef51234
StephenODea54 <odeastephen1@gmail.com>
2025-05-01 22:53:12 -0400

    add back standalone apollo server; add res and req to apollo context; add conditional cors attributes for dev

--- apollo/src/index.ts
+++ apollo/src/index.ts
@@ -1,110 +1,79 @@
-import { ApolloServer, HeaderMap } from '@apollo/server';
-import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
-import { fromNodeHeaders, toNodeHandler } from 'better-auth/node';
-import express from 'express';
-import type { Request, Response } from 'express';
-import http from 'node:http';
+import { ApolloServer } from '@apollo/server';
 import { schema } from './graphql/index.js';
 import { auth } from './utils/auth.js';
-import { type DocumentNode, getOperationAST, GraphQLError, parse } from 'graphql';
+import { fromNodeHeaders } from 'better-auth/node';
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import cookieparser from 'cookie-parser';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const PORT = 4000;
 
 const app = express();
 const httpServer = http.createServer(app);
 
-app.all('/api/auth/*splat', toNodeHandler(auth));
-
-const apollo = new ApolloServer({
+const serverPlugins = [ApolloServerPluginDrainHttpServer({ httpServer })];
+if (process.env.NODE_ENV === 'dev') {
+    serverPlugins.push(ApolloServerPluginLandingPageLocalDefault({ includeCookies: true }));
+}
+const server = new ApolloServer({
     schema,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    plugins: serverPlugins,
 });
 
-// I am beginning to regret not putting the auth into its own server.
-const createContext = async ({ req }: { req: Request }) => {
-    const body = req.body;
+await server.start();
 
-    /* Do not allow non-graphql operations to hit the server */
-    let parsedQuery: DocumentNode;
-    try {
-        parsedQuery = parse(body.query);
-    } catch {
-        throw new GraphQLError('Invalid GraphQL query', {
-            extensions: { code: 'BAD_REQUEST', http: { status: 400 } },
-        });
-    }
+const middlewares = [];
 
-    const operationAST = getOperationAST(parsedQuery, body.operationName);
+if (process.env.NODE_ENV === 'dev') {
+    middlewares.push(
+        cors<cors.CorsRequest>({
+            origin: ['https://sandbox.embed.apollographql.com'],
+            credentials: true,
+        }),
+    );
+}
 
-    /* Apollo server runs introspection queries that are only used to update
-       the schema within the UI. This allows introspection queries to come
-       through by explicitly setting to auth context to nothing. */
-    if (operationAST?.operation === 'query' && body.query.includes('__schema')) {
-        return { user: null, session: null };
-    }
+middlewares.push(
+    express.json(),
+    cookieparser(),
+    expressMiddleware(server, {
+        context: async ({ req, res }) => {
+            const headers = fromNodeHeaders(req.headers);
 
-    const session = await auth.api.getSession({
-        headers: fromNodeHeaders(req.headers),
-    });
+            const session = await auth.api.getSession({
+                headers: headers,
+            });
 
-    if (!session) {
-        throw new GraphQLError('Authentication token is invalid', {
-            extensions: {
-                code: 'UNAUTHENTICATED',
-                http: { status: 401 },
-            },
-        });
-    }
-
-    return {
-        headers: req.headers,
-        user: session.user,
-        session: session.session,
-    };
-};
-
-await apollo.start();
-app.use('/graphql', express.json(), async (req: Request, res: Response) => {
-    try {
-        const httpGraphQLResponse = await apollo.executeHTTPGraphQLRequest({
-            httpGraphQLRequest: {
-                body: req.method === 'POST' ? await req.body : undefined,
-                headers: new HeaderMap(
-                    Object.entries(req.headers).map(([key, value]) => [
-                        key,
-                        Array.isArray(value) ? value.join(', ') : value || '',
-                    ]),
-                ),
-                method: req.method,
-                search: new URL(req.url, `http://${req.headers.host}`).search,
-            },
-            context: async () => createContext({ req }),
-        });
-
-        const { headers, body, status } = httpGraphQLResponse;
-
-        for (const [headerKey, headerValue] of headers) {
-            res.setHeader(headerKey, headerValue);
-        }
-
-        res.status(status ?? 200);
-
-        if (body.kind === 'complete') {
-            res.send(body.string);
-        }
-
-        // TOOD: Might not need this, but leaving to be safe
-        if (body.kind === 'chunked') {
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            for await (const chunk of body.asyncIterator) {
-                res.write(chunk);
+            if (session?.session && session?.user) {
+                return {
+                    headers: headers,
+                    res: res,
+                    session: session?.session,
+                    user: session?.user,
+                };
             }
-            res.end();
-        }
-    } catch (error) {
-        res.status(500).json({ error });
-    }
-});
+
+            return {
+                headers: headers,
+                res: res,
+                session: undefined,
+                user: {
+                    userId: undefined,
+                },
+            };
+        },
+    }),
+);
+
+// @ts-expect-error middleware works fine, typescript is just inferring package types incorrectly
+app.use('/graphql', ...middlewares);
 
 await new Promise<void>((resolve) => httpServer.listen({ port: PORT }, resolve));
-console.log(`🚀 Server ready at http://localhost:${PORT}`);
+console.log(`🚀 Server ready at http://localhost:${PORT}/graphql`);
