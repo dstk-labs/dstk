695b7da526c536955cd286fc8fbfd148dc41e287
StephenODea54 <odeastephen1@gmail.com>
2025-05-20 13:50:37 -0400

    change delimiter from '*' to '|' so better parse date fields for encoder

--- apollo/src/utils/encoder.ts
+++ apollo/src/utils/encoder.ts
@@ -1,11 +1,13 @@
 export class Encoder {
+    DELIMETER = '|';
+
     // TODO: Change this to include more fields
     encode(...args: (string | number)[]) {
-        return Buffer.from(args.join(':'), 'utf8').toString('base64');
+        return Buffer.from(args.join(this.DELIMETER), 'utf8').toString('base64');
     }
 
     // TODO: Change this when more fields are included
     decode(encodedString: string) {
-        return Buffer.from(encodedString, 'base64').toString('utf8').split(':');
+        return Buffer.from(encodedString, 'base64').toString('utf8').split(this.DELIMETER);
     }
 }
