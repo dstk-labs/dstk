595a94760a14c77520b11bbb9d57ff62a010fee4
odeastephen1@gmail.com <s.odea@trustetc.com>
2025-06-05 23:32:37 -0400

    add API_URL to env config

---
+++ web/src/config/env.ts
@@ -0,0 +1,30 @@
+import { z } from 'zod';
+
+const EnvSchema = z.object({
+  API_URL: z.string().default('http://localhost:4000/graphql'),
+});
+
+const envVars = Object.entries(import.meta.env).reduce<
+  Record<string, string>
+>((acc, curr) => {
+  const [key, value] = curr;
+  if (key.startsWith('VITE_APP_')) {
+    acc[key.replace('VITE_APP_', '')] = value;
+  }
+  return acc;
+}, {});
+
+const parsedEnv = EnvSchema.safeParse(envVars);
+
+if (!parsedEnv.success) {
+  throw new Error(
+    `Invalid env provided.
+The following variables are missing or invalid:
+${Object.entries(parsedEnv.error.flatten().fieldErrors)
+  .map(([k, v]) => `- ${k}: ${v}`)
+  .join('\n')}
+`,
+  );
+}
+
+export const { API_URL } = parsedEnv.data;
