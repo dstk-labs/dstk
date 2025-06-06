bd8515947b5c3d7ecde996e0c103d4b103299041
odeastephen1@gmail.com <s.odea@trustetc.com>
2025-06-05 22:58:26 -0400

    add stylistic and perfectionist to eslint config

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -3,11 +3,16 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
+import perfectionist from 'eslint-plugin-perfectionist'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -16,6 +21,8 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      '@stylistic': stylistic,
+      perfectionist,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +30,21 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+
+      '@stylistic/semi': ['error', 'always'],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/indent': ['error', 2],
+
+      'perfectionist/sort-imports': [
+        'error',
+        {
+          type: 'module',
+          order: 'asc',
+          'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        },
+      ],
+      'perfectionist/sort-objects': ['warn', { order: 'asc' }],
+      'perfectionist/sort-array-includes': 'warn',
     },
-  },
+  }
 )
