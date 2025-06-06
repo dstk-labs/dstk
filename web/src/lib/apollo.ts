fed350cd9ea7745d2c5d33558bae0e5a71aa11c4
odeastephen1@gmail.com <s.odea@trustetc.com>
2025-06-05 23:39:09 -0400

    configure apollo client

---
+++ web/src/lib/apollo.ts
@@ -0,0 +1,42 @@
+import {
+  ApolloClient,
+  createHttpLink,
+  createQueryPreloader,
+  from,
+  InMemoryCache,
+} from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+import { notifications } from '@mantine/notifications';
+import { API_URL } from '../config/env';
+
+const httpLink = createHttpLink({
+  credentials: 'include',
+  uri: API_URL,
+});
+
+// TODO: Revisit for error categorization? Probably moot but not dismissing just yet.
+// TODO: 401 Errors should probably navigate to home
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(error => notifications.show({
+      title: 'Error',
+      message: error.message,
+      color: 'red',
+    }));
+  }
+
+  if (networkError) {
+    notifications.show({
+      title: networkError.name,
+      message: networkError.message,
+      color: 'red',
+    });
+  }
+});
+
+export const apolloClient = new ApolloClient({
+  link: from([errorLink, httpLink]),
+  cache: new InMemoryCache(),
+});
+
+export const preloadQuery = createQueryPreloader(apolloClient);
