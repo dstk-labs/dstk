63ef629aa1063f5493de66ab265c9c104a1b697d
StephenODea54 <odeastephen1@gmail.com>
2025-06-07 00:41:26 -0400

    create css file for auth layout; add horizontal scroll for footer on mobile

--- web/src/layouts/auth/authLayout.tsx
+++ web/src/layouts/auth/authLayout.tsx
@@ -1,10 +1,11 @@
 import { DotIcon, MoonIcon, SunIcon } from 'lucide-react';
-import { AppShell, Center, Group, useMantineColorScheme } from '@mantine/core';
+import { AppShell, Center, Group, ScrollArea, useMantineColorScheme } from '@mantine/core';
 import { Anchor } from '@/components/anchor/anchor';
 import { Outlet } from 'react-router';
 import { Fragment } from 'react/jsx-runtime';
 import { Logo } from '@/components/logo/logo';
 import { paths } from '@/config/paths';
+import styles from './authLayout.module.css';
 
 const footerLinks = [
   { link: paths.root.landing.path, label: `© ${new Date().getFullYear()} DSTK Labs.`},
@@ -29,43 +30,46 @@ export const AuthLayout = () => {
         </Center>
       </AppShell.Header>
       <AppShell.Main>
-        <Center h="calc(100vh - var(--app-shell-header-height, 0px) - var(--app-shell-footer-height, 0px))">
+        <Center className={styles.main}>
           <Outlet />
         </Center>
       </AppShell.Main>
       <AppShell.Footer>
         <Center h="100%">
-          <Group>
-            {footerLinks.map(footerLink => (
-              <Fragment key={footerLink.link}>
-                <Anchor
-                  c="gray"
-                  size="xs"
-                  to={footerLink.link}
-                >
-                  {footerLink.label}
-                </Anchor>
-                <Anchor
-                  c="gray"
-                  component="li"
-                  underline="never"
-                >
-                  <DotIcon size={14} />
-                </Anchor>
-              </Fragment>
-            ))}
-            <Anchor
-              c="gray"
-              component="button"
-              underline="never"
-            >
-              {colorScheme === 'light' ? (
-                <MoonIcon onClick={() => setColorScheme('dark')} size={14} />
-              ) : (
-                <SunIcon onClick={() => setColorScheme('light')} size={14} />
-              )}
-            </Anchor>
-          </Group>
+          <ScrollArea py="lg">
+            <Group px="lg" wrap="nowrap">
+              {footerLinks.map(footerLink => (
+                <Fragment key={footerLink.link}>
+                  <Anchor
+                    c="gray"
+                    className={styles.anchor}
+                    size="xs"
+                    to={footerLink.link}
+                  >
+                    {footerLink.label}
+                  </Anchor>
+                  <Anchor
+                    c="gray"
+                    component="li"
+                    underline="never"
+                  >
+                    <DotIcon size={14} />
+                  </Anchor>
+                </Fragment>
+              ))}
+              <Anchor
+                c="gray"
+                component="button"
+                underline="never"
+              >
+                {colorScheme === 'light' ? (
+                  <MoonIcon onClick={() => setColorScheme('dark')} size={14} />
+                ) : (
+                  <SunIcon onClick={() => setColorScheme('light')} size={14} />
+                )}
+              </Anchor>
+            </Group>
+          </ScrollArea>
         </Center>
       </AppShell.Footer>
     </AppShell>
