72b100c21cbac20f35c6d7a812a4980725886b10
Christopher Wetherill <1910269+wetherc@users.noreply.github.com>
2025-06-09 00:42:43 +0000

    Fixes for dev environment (#140)
    This just tidies up a couple broken things for dev builds. Notably:
    
      - Deleted an errant `dstk-web` subdirectory;
      - Updated `skaffold.yml` to reference the correct path for web builds;
      - Included some missing package dependencies;
      - Modified a gql codegen configuration to correct an `erasableSyntaxOnly` violation in how the enums were being synthesized;
      - Corrected a URI in the README

--- web/src/config/codegen.ts
+++ web/src/config/codegen.ts
@@ -9,10 +9,19 @@ const config: CodegenConfig = {
       preset: 'client',
       presetConfig: {
         gqlTagName: 'gql'
+      },
+      // Add this config block to instruct the underlying typescript plugin
+      // to generate enums as string literal union types.
+      config: {
+        enumsAsTypes: true
       }
     },
     './src/graphql/types.ts': {
       plugins: ['typescript', 'typescript-operations'],
+      // Also apply the same config here for consistency
+      config: {
+        enumsAsTypes: true
+      }
     },
   },
   ignoreNoDocuments: true,
