b80e1de8e4fda8118137785cda095cf55f95ddbf
Stephen O'Dea <odeastephen1@gmail.com>
2026-03-28 16:10:07 -0400

    Add Port, CORS, Better Auth, and App URL env vars (#166)

--- apollo/src/db/kysely.ts
+++ apollo/src/db/kysely.ts
@@ -1,12 +1,10 @@
 import type { DB } from "./db.js";
-import dotenv from "dotenv";
 
 import { Kysely, PostgresDialect } from "kysely";
 // https://github.com/brianc/node-postgres/issues/2819
 import pg from "pg";
 import { env } from "../config/env.js";
 
-dotenv.config();
 const { Pool } = pg;
 
 export const pool = new Pool({
