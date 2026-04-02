29aa6e1166694804fe007f62a28ea8b6c55bbd59
Stephen O'Dea <odeastephen1@gmail.com>
2026-04-01 22:11:27 -0400

    clean up graphql builder, scalar types, and include refactored export… (#169)
    ### `builder.ts`
    - Extracted `Context` into its own type so other files can use it
    - Removed `user_id` from `User` type
    
    ### `misc/edges.ts`
    - Removed since we don't have the edge relationships table anymore
    
    ### `graphql/index.ts`
    - Added exports for the new invitiations model, and for the refactor of
    the user directory (project and teams are now in their own folder)
    - None of these are actually PR'ed yet, just figured I would lob this in
    since it's related to the other Pothos changes

--- apollo/src/graphql/index.ts
+++ apollo/src/graphql/index.ts
@@ -1,12 +1,16 @@
 // Alphabetize plz
 import { builder } from "../builder.js";
 
+export * from "./auth/auth.js";
 export * from "./auth/authMutations.js";
 
+export * from "./invitation/index.js";
 export * from "./misc/index.js";
 export * from "./model-version/index.js";
 export * from "./model/index.js";
+export * from "./project/index.js";
 export * from "./storage-provider/index.js";
+export * from "./team/index.js";
 export * from "./user/index.js";
 
 export const schema = builder.toSchema();
--- apollo/src/graphql/misc/limit.ts
+++ apollo/src/graphql/misc/limit.ts
@@ -5,12 +5,9 @@ import { InputError } from "../../utils/errors.js";
 
 builder.scalarType("Limit", {
   serialize: n => n,
-  // @ts-expect-error This is a bug, according to the
-  // docs this should resolve correctly
-  // https://pothos-graphql.dev/docs/guide/scalars#defining-your-own-scalars
-  parseValue: (n: Limit) => {
-    if (LIMITS.includes(n)) {
-      return n;
+  parseValue: (n: unknown) => {
+    if (typeof n === "number" && LIMITS.includes(n)) {
+      return n as Limit;
     }
 
     throw new InputError({ name: "INVALID_LIMIT_ERROR" });
