281300c7e4ee278e9c9739deaaa742eb4c23e97a
StephenODEA54 <odeastephen1@gmail.com>
2026-09-14 15:34:12 -0400

    feat(web): add mantine theme and design tokens

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,5 @@
-import { AppProvider } from "./Provider";
-import { AppRouter } from "./Router";
+import { AppProvider } from "./provider";
+import { AppRouter } from "./router";
 
 export function App() {
   return (
